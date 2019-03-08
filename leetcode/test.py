983b6b10cb3c5698f5cbe0e1f629336f51a7db40
yangxiao <595705118@qq.com>
2019-03-08 15:48:10 +0800

    add top100

--- leetcode/test.py
+++ leetcode/test.py
@@ -2,5 +2,7 @@ from leetcode.top100 import Solution
 
 if __name__ == '__main__':
     obj = Solution()
+    root = obj.buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7])
+    print(obj.levelOrder(root))
     # print(obj.twoSum([1, 2, 3, 7], 8))
-    obj.printList(obj.createList([1, 2, 3, 4]))
+    # obj.printList(obj.lengthOfLongestSubstring('abcabcbb'))
